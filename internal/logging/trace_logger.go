8d793d03bbac48cd000787d8101b4667a77f5214
Adam Margherio <amargherio@microsoft.com>
2024-10-31 17:02:21 -0500

    Changing how the custom logger is getting span information.
    The provided context isn't going to be associated with a given span, so we need to pass in the span context to the logger as a field during each log call
    
    taking out the span.IsValid() call

--- internal/logging/trace_logger.go
+++ internal/logging/trace_logger.go
@@ -2,15 +2,16 @@ package logging
 
 import (
 	"context"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
 type TraceCore struct {
-	zapcore.Core
-	Ctx *context.Context
-	tp  trace.TracerProvider
+	ioCore zapcore.Core
+	Ctx    *context.Context
+	tp     trace.TracerProvider
 }
 
 // NewTraceCore Returns a new Core that adds tracing information to the log entry
@@ -18,18 +19,17 @@ func NewTraceCore(c zapcore.Core, ctx *context.Context, tp trace.TracerProvider)
 	return &TraceCore{c, ctx, tp}
 }
 
+func (c *TraceCore) Enabled(lvl zapcore.Level) bool {
+	return c.ioCore.Enabled(lvl)
+}
+
 // With adds structured context to the Core
 func (c *TraceCore) With(fields []zapcore.Field) zapcore.Core {
-	return &TraceCore{
-		Core: c.Core.With(fields),
-		Ctx:  c.Ctx,
-		tp:   c.tp,
-	}
+	return c.ioCore.With(fields)
 }
 
 func (c *TraceCore) Check(e zapcore.Entry, ce *zapcore.CheckedEntry) *zapcore.CheckedEntry {
-	if ce := c.Core.Check(e, ce); ce != nil {
-
+	if c.Enabled(e.Level) {
 		return ce.AddCore(e, c)
 	}
 	return ce
@@ -37,24 +37,47 @@ func (c *TraceCore) Check(e zapcore.Entry, ce *zapcore.CheckedEntry) *zapcore.Ch
 
 // Write serializes the Entry and any Fields, along with the trace information, to the Core
 func (c *TraceCore) Write(entry zapcore.Entry, fields []zapcore.Field) error {
-	// Get the current span from the context, if there is one
-	if span := trace.SpanFromContext(*c.Ctx); span != nil {
-		spanContext := span.SpanContext()
-		if spanContext.IsValid() {
-			fields = append(
-				fields,
-				zap.String("traceID", spanContext.TraceID().String()),
-				zap.String("spanID", spanContext.SpanID().String()),
-			)
+	// 1. get the supplied context from the fields (or the span if the context doesn't cut it).
+	// 2. if context, get the span from the context
+	// 3. using the span, add the trace ID, span ID, and name to the logged fields
+	// 4. write the entry to the core
 
-			if entry.Level == zapcore.DebugLevel {
-				fields = append(fields, zap.Any("spanContext", spanContext))
-			}
+	// Get the current span from the context, if there is one
+	var sc context.Context
+	var activeSpan trace.Span
+	for _, field := range fields {
+		if field.Key == "traceCtx" {
+			sc = field.Interface.(context.Context)
+			break
 		}
 	}
-	return c.Core.Write(entry, fields)
+	if sc == nil {
+		return c.ioCore.Write(entry, fields)
+	}
+	activeSpan = trace.SpanFromContext(sc)
+
+	// if we still didn't get an active span, skip those extra fields and write the entry
+	// todo: should we also check if the active span is recording here?
+	if activeSpan != nil && activeSpan.SpanContext().IsValid() {
+		ros := activeSpan.(sdktrace.ReadOnlySpan)
+		fields = append(
+			fields,
+			zap.String("traceID", ros.SpanContext().TraceID().String()),
+			zap.String("spanID", ros.SpanContext().SpanID().String()),
+			zap.String("spanName", ros.Name()),
+		)
+
+		if entry.Level == zapcore.DebugLevel {
+			fields = append(fields,
+				zap.Any("spanContext", ros.SpanContext()),
+				zap.Any("spanAttributes", ros.Attributes()),
+				zap.Any("rawSpan", ros),
+			)
+		}
+	}
+	return c.ioCore.Write(entry, fields)
 }
 
 func (c *TraceCore) Sync() error {
-	return c.Core.Sync()
+	return c.ioCore.Sync()
 }
